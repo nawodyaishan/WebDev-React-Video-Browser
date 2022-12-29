e97db63c9c0f22b7e68d2dfe5866cd75ac8fb128
nawodyaishan <nawodyain@gmail.com>
2022-12-30 01:12:52 +0530

    Add index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+// Creating component to generate html
+const App = () => {
+    return <div>Hi!!</div>
+}
+
+// Taking HTML JSX to the DOM
+ReactDOM.render(<App/>);
