57d97a0bc7bf07d2852320a81531fbb7262b44c4
nawodyaishan <nawodyain@gmail.com>
2023-01-12 13:01:05 +0530

    Add video list props

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,6 +1,7 @@
 import React, {Components} from 'react';
 
-const VideoList = () => {
+const VideoList = (props) => {
+    const = props.videos;
     return (
         <ul className={"col-md-4, list-group"}>
         </ul>
