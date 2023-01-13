b0cdf12ecd3a65705dbaebad33e1c83927456598
nawodyaishan <nawodyain@gmail.com>
2023-01-13 18:54:44 +0530

    Add video list item style

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,8 +1,22 @@
 import React, {Component} from 'react';
 
-const VideoListItem = (props) => {
+const VideoListItem = ({video}) => {
+    // grabs video props and create same variable const video = props.video;
+
     return (
-        <li>Video</li>
+        <li className={"list-group-item"}>
+            <div className={"video-list-media"}>
+                <div className={"media-left"}>
+                    <img className={"media-object"}/>
+                </div>
+
+                <div className={"media-body"}>
+                    <div className={"media-heading"}>
+
+                    </div>
+                </div>
+            </div>
+        </li>
     )
 }
 
