244baa09d2b0135bc069442d4806eb2845246a21
nawodyaishan <nawodyain@gmail.com>
2023-01-13 23:18:13 +0530

    Add onInputchange fn

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -9,10 +9,15 @@ class SearchBar extends Component {
 
     render() {
         return (<div className={"search-bar"}>
-                <input
-                    value={this.state.term}
-                    onChange={event => this.setState({term: event.target.value})}/>
-            </div>);
+            <input
+                value={this.state.term}
+                onChange={event => this.onInputChange(event.target.value)}/>
+        </div>);
+    }
+
+    onInputChange(term) {
+        this.setState({term});
+        this.props.onSearchTermChange(term);
     }
 }
 
