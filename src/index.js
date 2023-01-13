a70ad62ae66e861bdec9a37e1fff750cc1678530
nawodyaishan <nawodyain@gmail.com>
2023-01-13 23:27:39 +0530

    Add lodash fn

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
+import _ from "lodash";
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
 import YTSearch from "youtube-api-search";
 import SearchBar from "./components/search_bar";
 import VideoList from "./components/video_list";
 import VideoDetail from "./components/video_detail";
-import _ from "lodash";
+
 
 const API_KEY = "AIzaSyAwmN7Zhq3mNwYXqiaAMMg_fCPCej74sIw";
 
@@ -28,8 +29,12 @@ class App extends Component {
     }
 
     render() {
+        const videoSearch = _.debounce((term) => {
+            this.videoSearch(term)
+        }, 300)
+
         return (<div>
-            <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+            <SearchBar onSearchTermChange={videoSearch}/>
             <VideoDetail video={this.state.selectedVideo}/>
             <VideoList
                 onVideoSelect={(selectedVideo) => this.setState({selectedVideo})}
