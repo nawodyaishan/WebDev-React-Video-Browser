6b9d54fb05b9e8557ac5ce059cf353d280020ebf
nawodyaishan <nawodyain@gmail.com>
2023-01-13 23:19:50 +0530

    Add lodash

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from "youtube-api-search";
 import SearchBar from "./components/search_bar";
 import VideoList from "./components/video_list";
 import VideoDetail from "./components/video_detail";
+import _ from "lodash";
 
 const API_KEY = "AIzaSyAwmN7Zhq3mNwYXqiaAMMg_fCPCej74sIw";
 
